0d818bf19ef9f7f3b2467d82d0384eb69f83d7c5
Yukio Siraichi <yukio.siraichi@gmail.com>
2017-04-17 03:55:34 -0300

    CommandLine support finished.

--- include/enfield/Support/CommandLine.h
+++ include/enfield/Support/CommandLine.h
@@ -2,6 +2,7 @@
 #define __EFD_COMMAND_LINE_H__
 
 #include <string>
+#include <cassert>
 
 namespace efd {
 
@@ -12,7 +13,7 @@ namespace efd {
 
             OptBase(std::string name, std::string description);
 
-            virtual void setVal(std::string valStr);
+            virtual void parse(int argc, char **argv) = 0;
     };
 
     template <typename T>
@@ -24,12 +25,35 @@ namespace efd {
             Opt(std::string name, std::string description);
             Opt(std::string name, std::string description, T def);
 
-            T& getVal() const;
-            void setVal(std::string valStr) override;
+            const T& getVal() const;
+            void parse(int argc, char **argv) override;
     };
 
     void ParseArguments(int argc, char **argv);
 
+    template class Opt<bool>;
+    template <> void Opt<bool>::parse(int argc, char **argv);
+
+    template class Opt<int>;
+    template <> void Opt<int>::parse(int argc, char **argv);
+
+    template class Opt<unsigned>;
+    template <> void Opt<unsigned>::parse(int argc, char **argv);
+
+    template class Opt<long long>;
+    template <> void Opt<long long>::parse(int argc, char **argv);
+
+    template class Opt<unsigned long long>;
+    template <> void Opt<unsigned long long>::parse(int argc, char **argv);
+
+    template class Opt<float>;
+    template <> void Opt<float>::parse(int argc, char **argv);
+
+    template class Opt<double>;
+    template <> void Opt<double>::parse(int argc, char **argv);
+
+    template class Opt<std::string>;
+    template <> void Opt<std::string>::parse(int argc, char **argv);
 };
 
 template <typename T>
@@ -39,8 +63,11 @@ template <typename T>
 efd::Opt<T>::Opt(std::string name, std::string description, T def) : OptBase(name, description), mVal(def) {}
 
 template <typename T>
-T& efd::Opt<T>::getVal() const {
-    return mVal;
+const T& efd::Opt<T>::getVal() const { return mVal; }
+
+template <typename T>
+void efd::Opt<T>::parse(int argc, char **argv) {
+    assert(false && "Option with 'parse' function not implemented.");
 }
 
 #endif
--- lib/Support/CommandLine.cpp
+++ lib/Support/CommandLine.cpp
@@ -1,9 +1,14 @@
 
 #include "enfield/Support/CommandLine.h"
 
+#include <cstdlib>
+
+#include <iostream>
+#include <iomanip>
+
 #include <unordered_map>
+#include <vector>
 #include <memory>
-#include <cassert>
 
 using namespace efd;
 
@@ -11,9 +16,9 @@ namespace efd {
 
     class ArgsParser {
         public:
-            std::unordered_map<std::string, OptBase*> mArgMap;
+            std::unordered_map<std::string, std::vector<OptBase*> > mArgMap;
 
-            ArgsParser();
+            ArgsParser() {}
 
             bool hasOpt(OptBase* opt);
             void addOpt(OptBase* opt);
@@ -21,17 +26,21 @@ namespace efd {
 
 };
 
-ArgsParser::ArgsParser() {}
-
 bool ArgsParser::hasOpt(OptBase* opt) {
     return mArgMap.find(opt->mName) != mArgMap.end();
 }
 
 void ArgsParser::addOpt(OptBase* opt) {
-    mArgMap.insert(std::pair<std::string, OptBase*>(opt->mName, opt));
+    if (!hasOpt(opt)) {
+        std::string name(opt->mName);
+        mArgMap.insert(std::make_pair(name, std::vector<OptBase*>()));
+    }
+
+    mArgMap[opt->mName].push_back(opt);
 }
 
 static std::shared_ptr<ArgsParser> Parser(nullptr);
+static efd::Opt<bool> PrintHelp("help", "Prints a list with the available commands.", false);
 
 static std::shared_ptr<ArgsParser> getParser() {
     if (Parser.get() == nullptr)
@@ -40,23 +49,88 @@ static std::shared_ptr<ArgsParser> getParser() {
     return Parser;
 }
 
-OptBase::OptBase(std::string name, std::string description) {
-    std::shared_ptr<ArgsParser> Parser = getParser();
-
-    if (!Parser->hasOpt(this))
-        Parser->addOpt(this);
+template <>
+void efd::Opt<bool>::parse(int argc, char **argv) {
+    mVal = true;
 }
 
-void ParseArguments(int argc, char **argv) {
+template <>
+void efd::Opt<int>::parse(int argc, char **argv) {
+    assert(argc > 2 && "Error parsing the arguments.");
+    mVal = std::stoi(std::string(argv[1]));
+}
+
+template <>
+void efd::Opt<unsigned>::parse(int argc, char **argv) {
+    assert(argc > 2 && "Error parsing the arguments.");
+    mVal = std::stoul(std::string(argv[1]));
+}
+
+template <>
+void efd::Opt<long long>::parse(int argc, char **argv) {
+    assert(argc > 2 && "Error parsing the arguments.");
+    mVal = std::stoll(std::string(argv[1]));
+}
+
+template <>
+void efd::Opt<unsigned long long>::parse(int argc, char **argv) {
+    assert(argc > 2 && "Error parsing the arguments.");
+    mVal = std::stoull(std::string(argv[1]));
+}
+
+template <>
+void efd::Opt<float>::parse(int argc, char **argv) {
+    assert(argc > 2 && "Error parsing the arguments.");
+    mVal = std::stof(std::string(argv[1]));
+}
+
+template <>
+void efd::Opt<double>::parse(int argc, char **argv) {
+    assert(argc > 2 && "Error parsing the arguments.");
+    mVal = std::stod(std::string(argv[1]));
+}
+
+template <>
+void efd::Opt<std::string>::parse(int argc, char **argv) {
+    assert(argc > 2 && "Error parsing the arguments.");
+    mVal = std::string(argv[1]);
+}
+
+efd::OptBase::OptBase(std::string name, std::string description) : mName(name), mDescription(description) {
+    std::shared_ptr<ArgsParser> Parser = getParser();
+    Parser->addOpt(this);
+}
+
+static void PrintCommandLineHelp() {
+    std::shared_ptr<ArgsParser> Parser = getParser();
+
+    std::cout << "Arguments:" << std::endl;
+
+    const int nCols = 20;
+    for (auto pair : Parser->mArgMap) {
+        std::cout << "\t" << std::left << std::setw(nCols) << "-" + pair.first;
+        std::cout << pair.second[0]->mDescription << std::endl;
+    }
+}
+
+void efd::ParseArguments(int argc, char **argv) {
     std::shared_ptr<ArgsParser> Parser = getParser();
 
     for (int i = 1; i < argc; ++i) {
-        std::string arg(argv[i]);
+        // Removing the initial '-'
+        char *argName = argv[i] + 1;
+        std::string arg(argName);
+
+        char **argvI = argv + i;
+        int argcI = argc - i;
 
         if (Parser->mArgMap.find(arg) != Parser->mArgMap.end()) {
+            for (OptBase *opt : Parser->mArgMap[arg])
+                opt->parse(argcI, argvI);
         }
     }
 
-    for (auto pair : Parser->mArgMap) {
+    if (PrintHelp.getVal()) {
+        PrintCommandLineHelp();
     }
 }
