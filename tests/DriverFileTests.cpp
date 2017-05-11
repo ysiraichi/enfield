943b4e39c52b2e8d628536ccc16efbe2c120ffda
Yukio Siraichi <yukio.siraichi@gmail.com>
2017-05-11 02:43:12 -0300

    Parsing from file tests passing.

--- tests/DriverFileTests.cpp
+++ tests/DriverFileTests.cpp
@@ -3,10 +3,29 @@
 #include "enfield/Analysis/Driver.h"
 #include "enfield/Support/RTTI.h"
 
+#include <vector>
+#include <fstream>
 #include <string>
 
 using namespace efd;
 
+const std::string dir = "files/";
+const std::vector<std::string> files = {
+    "_adder.qasm",
+    "_bigadder.qasm",
+    "_inverseqft1.qasm",
+    "_inverseqft2.qasm",
+    "_qec.qasm",
+    "_qft.qasm",
+    "_qpt.qasm",
+    "_rb.qasm",
+    "_teleport.qasm",
+    "_W-state.qasm"
+};
+
 TEST(DriverFileTests, ParsingFilesTest) {
-    
+    for (const std::string& file : files) {
+        NodeRef root = efd::ParseFile(file, dir);
+        ASSERT_FALSE(root == nullptr);
+    }
 }
