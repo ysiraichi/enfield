375ad97e0eccbcd34d85c9af2d42bf5bd257a57d
Yukio Siraichi <yukio.siraichi@gmail.com>
2017-05-16 17:43:01 -0300

    Created method init in 'NodeVisitor'.

--- lib/Analysis/NodeVisitor.cpp
+++ lib/Analysis/NodeVisitor.cpp
@@ -1,23 +1,25 @@
 #include "enfield/Analysis/NodeVisitor.h"
 
-void efd::NodeVisitor::visit(NDQasmVersion* v) {}
-void efd::NodeVisitor::visit(NDInclude* v) {}
-void efd::NodeVisitor::visit(NDDecl* v) {}
-void efd::NodeVisitor::visit(NDGateDecl* v) {}
-void efd::NodeVisitor::visit(NDOpaque* v) {}
-void efd::NodeVisitor::visit(NDQOpMeasure* v) {}
-void efd::NodeVisitor::visit(NDQOpReset* v) {}
-void efd::NodeVisitor::visit(NDQOpU* v) {}
-void efd::NodeVisitor::visit(NDQOpCX* v) {}
-void efd::NodeVisitor::visit(NDQOpBarrier* v) {}
-void efd::NodeVisitor::visit(NDQOpGeneric* v) {}
-void efd::NodeVisitor::visit(NDBinOp* v) {}
-void efd::NodeVisitor::visit(NDUnaryOp* v) {}
-void efd::NodeVisitor::visit(NDIdRef* v) {}
-void efd::NodeVisitor::visit(NDList* v) {}
-void efd::NodeVisitor::visit(NDStmtList* v) {}
-void efd::NodeVisitor::visit(NDGOpList* v) {}
-void efd::NodeVisitor::visit(NDIfStmt* v) {}
-void efd::NodeVisitor::visit(NDValue<std::string>* v) {}
-void efd::NodeVisitor::visit(NDValue<IntVal>* v) {}
-void efd::NodeVisitor::visit(NDValue<RealVal>* v) {}
+void efd::NodeVisitor::visit(NDQasmVersion* ref) {}
+void efd::NodeVisitor::visit(NDInclude* ref) {}
+void efd::NodeVisitor::visit(NDDecl* ref) {}
+void efd::NodeVisitor::visit(NDGateDecl* ref) {}
+void efd::NodeVisitor::visit(NDOpaque* ref) {}
+void efd::NodeVisitor::visit(NDQOpMeasure* ref) {}
+void efd::NodeVisitor::visit(NDQOpReset* ref) {}
+void efd::NodeVisitor::visit(NDQOpU* ref) {}
+void efd::NodeVisitor::visit(NDQOpCX* ref) {}
+void efd::NodeVisitor::visit(NDQOpBarrier* ref) {}
+void efd::NodeVisitor::visit(NDQOpGeneric* ref) {}
+void efd::NodeVisitor::visit(NDBinOp* ref) {}
+void efd::NodeVisitor::visit(NDUnaryOp* ref) {}
+void efd::NodeVisitor::visit(NDIdRef* ref) {}
+void efd::NodeVisitor::visit(NDList* ref) {}
+void efd::NodeVisitor::visit(NDStmtList* ref) {}
+void efd::NodeVisitor::visit(NDGOpList* ref) {}
+void efd::NodeVisitor::visit(NDIfStmt* ref) {}
+void efd::NodeVisitor::visit(NDValue<std::string>* ref) {}
+void efd::NodeVisitor::visit(NDValue<IntVal>* ref) {}
+void efd::NodeVisitor::visit(NDValue<RealVal>* ref) {}
+
+void efd::NodeVisitor::init() {}
