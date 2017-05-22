8790183849476715198273be6e8466088f117e68
Yukio Siraichi <yukio.siraichi@gmail.com>
2017-05-22 00:43:17 -0300

    Inlining gates. Tests passing.

---
+++ tests/GateInlineTests.cpp
@@ -0,0 +1,105 @@
+
+#include "gtest/gtest.h"
+
+#include "enfield/Transform/GateInline.h"
+#include "enfield/Analysis/QModule.h"
+#include "enfield/Support/RTTI.h"
+
+#include <string>
+
+using namespace efd;
+
+TEST(GateInlineTests, IdInline) {
+    const std::string program =
+"\
+gate id a {\
+}\
+qreg q[5];\
+id q[0];\
+";
+
+    const std::string inlined =
+"\
+gate id a {\
+}\
+qreg q[5];\
+";
+
+    std::unique_ptr<QModule> qmod = QModule::ParseString(program);
+    for (auto it = qmod->stmt_begin(), e = qmod->stmt_end(); it != e; ++it)
+        if (NDQOpGeneric* qop = dynCast<NDQOpGeneric>(*it))
+            InlineGate(qmod.get(), qop);
+
+    ASSERT_EQ(qmod->toString(), inlined);
+}
+
+TEST(GateInlineTests, PrimitiveGatesInline) {
+    const std::string program =
+"\
+include \"files/qelib1.inc\";\
+gate my_ccx a, b, c {\
+h c;\
+cx b, c;\
+tdg c;\
+cx a, c;\
+t c;\
+cx b, c;\
+tdg c;\
+cx a, c;\
+t b;\
+t c;\
+h c;\
+cx a, b;\
+t a;\
+tdg b;\
+cx a, b;\
+}\
+qreg q[5];\
+my_ccx q[0], q[1], q[2];\
+";
+
+    const std::string inlined =
+"\
+include \"files/qelib1.inc\";\
+gate my_ccx a, b, c {\
+h c;\
+cx b, c;\
+tdg c;\
+cx a, c;\
+t c;\
+cx b, c;\
+tdg c;\
+cx a, c;\
+t b;\
+t c;\
+h c;\
+cx a, b;\
+t a;\
+tdg b;\
+cx a, b;\
+}\
+qreg q[5];\
+h q[2];\
+cx q[1], q[2];\
+tdg q[2];\
+cx q[0], q[2];\
+t q[2];\
+cx q[1], q[2];\
+tdg q[2];\
+cx q[0], q[2];\
+t q[1];\
+t q[2];\
+h q[2];\
+cx q[0], q[1];\
+t q[0];\
+tdg q[1];\
+cx q[0], q[1];\
+";
+
+    std::unique_ptr<QModule> qmod = QModule::ParseString(program);
+    for (auto it = qmod->stmt_begin(), e = qmod->stmt_end(); it != e; ++it)
+        if (NDQOpGeneric* qop = dynCast<NDQOpGeneric>(*it))
+            InlineGate(qmod.get(), qop);
+
+    ASSERT_EQ(qmod->toString(), inlined);
+}
