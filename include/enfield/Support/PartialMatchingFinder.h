b5943f391b24009b0f7dc2855b5852fd5f19ab50
Yukio Siraichi <yukio.siraichi@gmail.com>
2017-07-17 11:07:03 -0300

    Partial Matching related classes using smart pointer.

--- include/enfield/Support/PartialMatchingFinder.h
+++ include/enfield/Support/PartialMatchingFinder.h
@@ -2,12 +2,17 @@
 #define __EFD_PARTIAL_MATCHING_FINDER_H__
 
 #include <vector>
+#include <memory>
 
 namespace efd {
     /// \brief Interface for implementing different algorithms for finding
     /// partial matchings (approximate subgraph isomorphism).
     class PartialMatchingFinder {
         public:
+            typedef PartialMatchingFinder* Ref;
+            typedef std::unique_ptr<PartialMatchingFinder> uRef;
+            typedef std::shared_ptr<PartialMatchingFinder> sRef;
+
             /// \brief Returns a valid matching.
             /// Note that this is not necessairly an exact match (exact 
             /// subgraph isomorphism). That is because this is a NP-Complete
