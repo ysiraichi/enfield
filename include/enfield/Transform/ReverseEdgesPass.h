79c072df548e57f0a9e7a225758d93148b75f56c
Yukio Siraichi <yukio.siraichi@gmail.com>
2017-07-25 05:50:00 -0300

    ReverseEdgesPass refactored.

--- include/enfield/Transform/ReverseEdgesPass.h
+++ include/enfield/Transform/ReverseEdgesPass.h
@@ -1,36 +1,28 @@
 #ifndef __EFD_REVERSE_EDGES_PASS_H__
 #define __EFD_REVERSE_EDGES_PASS_H__
 
-#include "enfield/Pass.h"
+#include "enfield/Transform/Pass.h"
 #include "enfield/Transform/QModule.h"
 #include "enfield/Transform/DependencyBuilderPass.h"
 #include "enfield/Arch/ArchGraph.h"
 
 namespace efd {
     /// \brief Pass that reverses the edges accordingly with the architecture Graph.
-    class ReverseEdgesPass : public Pass {
+    class ReverseEdgesPass : public PassT<void> {
         public:
             typedef ReverseEdgesPass* Ref;
             typedef std::unique_ptr<ReverseEdgesPass> uRef;
 
         private:
             ArchGraph::sRef mG;
-            QModule::sRef mMod;
 
-            DependencyBuilderPass::uRef mDepPass;
-
-            ReverseEdgesPass(QModule::sRef qmod, ArchGraph::sRef graph);
-
-            void initImpl(bool force) override;
+            ReverseEdgesPass(ArchGraph::sRef graph);
 
         public:
-            void visit(NDQOpCX::Ref ref) override;
-            void visit(NDQOpGeneric::Ref ref) override;
-
-            bool doesInvalidatesModule() const override;
+            void run(QModule::Ref qmod) override;
 
             /// \brief Create an instance of this class.
-            static uRef Create(QModule::sRef qmod, ArchGraph::sRef graph);
+            static uRef Create(ArchGraph::sRef graph);
     };
 }
 
