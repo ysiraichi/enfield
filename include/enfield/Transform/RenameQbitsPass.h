0af2192e3c5e8820c96d4b0bb97dbc1508ba426f
Yukio Siraichi <yukio.siraichi@gmail.com>
2017-07-25 05:48:59 -0300

    RenameQbitPass refactored.

--- include/enfield/Transform/RenameQbitsPass.h
+++ include/enfield/Transform/RenameQbitsPass.h
@@ -1,7 +1,8 @@
 #ifndef __EFD_RENAME_QBIT_PASS_H__
 #define __EFD_RENAME_QBIT_PASS_H__
 
-#include "enfield/Pass.h"
+#include "enfield/Transform/Pass.h"
+#include "enfield/Transform/QModule.h"
 #include "enfield/Analysis/Nodes.h"
 
 #include <unordered_map>
@@ -11,7 +12,7 @@ namespace efd {
     class QbitToNumberPass;
 
     /// \brief Renames all the qbits according to the map from the constructor.
-    class RenameQbitPass : public Pass {
+    class RenameQbitPass : public PassT<void> {
         public:
             typedef RenameQbitPass* Ref;
             typedef std::unique_ptr<RenameQbitPass> uRef;
@@ -23,22 +24,8 @@ namespace efd {
 
             RenameQbitPass(ArchMap map);
 
-            /// \brief Gets the node associated with the old node (that is currently
-            /// inside)
-            Node::uRef getNodeFromOld(Node::Ref old);
-            /// \brief Returns true if this call is a call to the swap gate.
-            bool isSwapGate(NDQOpGeneric::Ref ref);
-
         public:
-            void visit(NDQOpMeasure::Ref ref) override;
-            void visit(NDQOpReset::Ref ref) override;
-            void visit(NDQOpU::Ref ref) override;
-            void visit(NDQOpCX::Ref ref) override;
-            void visit(NDQOpBarrier::Ref ref) override;
-            void visit(NDQOpGeneric::Ref ref) override;
-            void visit(NDList::Ref ref) override;
-
-            bool doesInvalidatesModule() const override;
+            void run(QModule::Ref qmod) override;
 
             /// \brief Creates a new instance of this pass.
             static RenameQbitPass::uRef Create(ArchMap map);
