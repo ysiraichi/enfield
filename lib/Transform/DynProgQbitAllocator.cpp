92c512da974c1e43b5b12955038538a840008fa7
Yukio Siraichi <yukio.siraichi@gmail.com>
2017-09-20 16:57:38 -0300

    [DynProgQbitAllocator]: using TSP (token swap problem) exponential algorithm.

--- lib/Transform/DynProgQbitAllocator.cpp
+++ lib/Transform/DynProgQbitAllocator.cpp
@@ -1,6 +1,7 @@
 #include "enfield/Transform/DynProgQbitAllocator.h"
 #include "enfield/Support/BFSPathFinder.h"
 #include "enfield/Support/CommandLine.h"
+#include "enfield/Support/ExpTSFinder.h"
 
 #include <unordered_map>
 #include <limits>
@@ -20,117 +21,6 @@ struct PermVal {
     unsigned idx;
     std::vector<unsigned> perm;
 };
-
-static std::unordered_map<std::string, PermVal> PermMap;
-static std::vector<std::vector<std::pair<unsigned, unsigned>>> swaps;
-    
-static std::string VecToKey(std::vector<unsigned> &v) {
-    std::string s("");
-    for (unsigned u : v)
-        s = s + "|" + std::to_string(u);
-    return s;
-}
-
-static void genPermutationMap(unsigned n) {
-    unsigned idx = 0;
-    std::vector<unsigned> perm(n, 0);
-    for (int i = 0; i < n; ++i) perm[i] = i;
-
-    do {
-        PermMap.insert(std::pair<std::string, PermVal>(VecToKey(perm), { idx++, perm }));
-    } while (next_permutation(perm.begin(), perm.end()));
-}
-
-static PermVal translateToPermVal
-(std::vector<unsigned>& source, std::vector<unsigned>& target) {
-    int size = source.size();
-    std::vector<unsigned> realTgt(size, 0);
-    std::vector<unsigned> inTranslator(size, 0);
-
-    for (int i = 0; i < size; ++i) {
-        inTranslator[source[i]] = i;
-    }
-
-    for (int i = 0; i < size; ++i) {
-        realTgt[i] = inTranslator[target[i]];
-    }
-
-    std::string tgtS = VecToKey(realTgt);
-    return PermMap[tgtS];
-}
-
-// Source and Target are Phys->Prog mapping.
-static unsigned getSwapNum
-(std::vector<unsigned>& source, std::vector<unsigned>& target) {
-    unsigned id = translateToPermVal(source, target).idx;
-    return swaps[id].size();
-}
-
-// Returns the swaps of the physical qubits.
-static std::vector<std::pair<unsigned, unsigned>>
-getSwaps(std::vector<unsigned>& source, std::vector<unsigned>& target) {
-    unsigned id = translateToPermVal(source, target).idx;
-    return swaps[id];
-    // return vector<pair<int, int>>();
-}
-
-// Pre-process the architechture graph, calculating the optimal swaps from every
-// permutation.
-static void computeSwaps(efd::Graph archG) {
-    int size = archG.size();
-    genPermutationMap(size);
-
-    std::vector<bool> inserted(PermMap.size(), false);
-    swaps.assign(PermMap.size(), std::vector<std::pair<unsigned, unsigned>>());
-
-    std::vector<PermVal*> ref(PermMap.size(), nullptr);
-    for (auto& pair : PermMap) {
-        ref[pair.second.idx] = &pair.second;
-    }
-
-    std::vector<unsigned> curPerm;
-    std::queue<unsigned> q;
-
-    // Initial permutation [0, 1, 2, 3, 4]
-    q.push(0);
-    inserted[0] = true;
-    while (!q.empty()) {
-        int pId = q.front();
-        q.pop();
-
-        PermVal& val = *ref[pId];
-
-        for (unsigned u = 0; u < size; ++u) {
-            for (unsigned v : archG.succ(u)) {
-                std::vector<unsigned> copy = val.perm;
-                std::swap(copy[u], copy[v]);
-
-                std::string key = VecToKey(copy);
-                int copyId = PermMap[key].idx;
-                if (!inserted[copyId]) {
-                    inserted[copyId] = true;
-                    swaps[copyId] = swaps[pId];
-                    swaps[copyId].push_back(std::pair<unsigned, unsigned>(u, v));
-                    q.push(copyId);
-                }
-            }
-
-            for (unsigned v : archG.pred(u)) {
-                std::vector<unsigned> copy = val.perm;
-                std::swap(copy[u], copy[v]);
-
-                std::string key = VecToKey(copy);
-                int copyId = PermMap[key].idx;
-                if (!inserted[copyId]) {
-                    inserted[copyId] = true;
-                    swaps[copyId] = swaps[pId];
-                    swaps[copyId].push_back(std::pair<unsigned, unsigned>(u, v));
-                    q.push(copyId);
-                }
-            }
-        }
-    }
-}
 
 static inline unsigned min(unsigned a, unsigned b) {
     if (a == UNREACH && b == UNREACH)
@@ -162,21 +52,22 @@ unsigned efd::DynProgQbitAllocator::getIntermediateV(unsigned u, unsigned v) {
 }
 
 efd::Solution efd::DynProgQbitAllocator::solve(DepsSet& deps) {
-    computeSwaps(*mArchGraph);
+    ExpTSFinder tsp(mArchGraph);
+    auto permutations = tsp.mAssigns;
 
     unsigned archQ = mArchGraph->size();
     const unsigned SWAP_COST = SwapCost.getVal();
     const unsigned REV_COST = RevCost.getVal();
     const unsigned LCX_COST = LCXCost.getVal();
 
-    int permN = PermMap.size();
+    int permN = permutations.size();
     int depN = deps.size();
 
     auto finder = BFSPathFinder::Create();
 
-    std::vector<std::vector<unsigned>*> permIdMap(permN, nullptr);
-    for (auto &pair : PermMap)
-        permIdMap[pair.second.idx] = &pair.second.perm;
+    // std::vector<std::vector<unsigned>*> permIdMap(permN, nullptr);
+    // for (auto &pair : PermMap)
+    //     permIdMap[pair.second.idx] = &pair.second.perm;
 
     // Map with the minimum number of vals at time 'i'.
     Val vals[permN][depN + 1];
@@ -195,7 +86,7 @@ efd::Solution efd::DynProgQbitAllocator::solve(DepsSet& deps) {
 
         for (unsigned tgt = 0; tgt < permN; ++tgt) {
             // Check if target tgtPermutation has the dependency required.
-            auto& tgtPerm = *permIdMap[tgt];
+            auto& tgtPerm = permutations[tgt];
             // Arch qubit interaction (u, v)
             unsigned u = tgtPerm[dep.mFrom], v = tgtPerm[dep.mTo];
 
@@ -218,9 +109,9 @@ efd::Solution efd::DynProgQbitAllocator::solve(DepsSet& deps) {
                 unsigned finalCost = srcVal.cost;
 
                 if (tgt != src) {
-                    auto srcAssign = GenAssignment(archQ, *permIdMap[src]);
+                    auto srcAssign = GenAssignment(archQ, permutations[src]);
                     auto tgtAssign = GenAssignment(archQ, tgtPerm);
-                    finalCost += getSwapNum(srcAssign, tgtAssign) * SWAP_COST;
+                    finalCost += tsp.find(srcAssign, tgtAssign).size() * SWAP_COST;
                 }
 
                 if (!hasEdge) {
@@ -256,7 +147,7 @@ efd::Solution efd::DynProgQbitAllocator::solve(DepsSet& deps) {
 
     for (int i = depN-1; i >= 0; --i) {
         assert(val->parent != nullptr && "Nullptr reached too soon.");
-        mappings[i] = std::make_pair(val->pId, *(permIdMap)[val->pId]);
+        mappings[i] = std::make_pair(val->pId, permutations[val->pId]);
         val = val->parent;
     }
 
@@ -278,9 +169,9 @@ efd::Solution efd::DynProgQbitAllocator::solve(DepsSet& deps) {
                 auto srcAssign = GenAssignment(archQ, src);
                 auto tgtAssign = GenAssignment(archQ, tgt);
 
-                auto swaps = getSwaps(srcAssign, tgtAssign);
-                for (auto pair : swaps) {
-                    unsigned u = pair.first, v = pair.second;
+                auto swaps = tsp.find(srcAssign, tgtAssign);
+                for (auto swp : swaps) {
+                    unsigned u = swp.u, v = swp.v;
 
                     if (mArchGraph->isReverseEdge(u, v))
                         std::swap(u, v);
