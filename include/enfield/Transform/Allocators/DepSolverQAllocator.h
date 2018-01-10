c4b8a52ee63012b056b71937da89a129d37c907a
Yukio Siraichi <yukio.siraichi@gmail.com>
2018-01-10 16:27:14 +0100

    Minor fixes.
    Declared 'typedef's for 'TokenSwapFinder'.
    Added description for 'DepSolverQAllocator'.
    Removed unused header inclusion from 'RandomMappingFinder'.

--- include/enfield/Transform/Allocators/DepSolverQAllocator.h
+++ include/enfield/Transform/Allocators/DepSolverQAllocator.h
@@ -4,14 +4,16 @@
 #include "enfield/Transform/Allocators/QbitAllocator.h"
 
 namespace efd {
+    /// \brief Wrapper for making it easier to implement algorithms for \em Qubit Allocation.
+    ///
+    /// It just gives a set of dependencies (note that it can be nested) and outputs a solution
+    /// for the current instance.
     class DepSolverQAllocator : public QbitAllocator {
         protected:
             DepSolverQAllocator(ArchGraph::sRef archGraph);
 
             /// \brief Solves the allocation problem, returning a solution.
             virtual Solution solve(DepsSet& deps) = 0;
-
-            /// \brief Executes the allocation algorithm after the preprocessing.
             virtual Solution executeAllocation(QModule::Ref qmod) override;
     };
 }
