ed9a0228624e1bd0af620b15fb9db1f641d93933
Yukio Siraichi <yukio.siraichi@gmail.com>
2018-01-10 16:14:36 +0100

    [SIFinder] 'PartialMatchingFinder' renamed to 'SIFinder'.
    'SIFinder' is more general (and shorter!). It encompasses as both
    algorithms that find some partial subgraph isomorphism and the exact
    solution.

---
+++ include/enfield/Support/SIFinder.h
@@ -0,0 +1,33 @@
+#ifndef __EFD_SI_FINDER_H__
+#define __EFD_SI_FINDER_H__
+
+#include "enfield/Support/Graph.h"
+#include "enfield/Support/Defs.h"
+
+#include <vector>
+#include <memory>
+
+namespace efd {
+    /// \brief Interface for implementing different algorithms for finding
+    /// matchings (subgraph isomorphism).
+    class SIFinder {
+        public:
+            typedef SIFinder* Ref;
+            typedef std::unique_ptr<SIFinder> uRef;
+            typedef std::shared_ptr<SIFinder> sRef;
+
+            struct Result {
+                bool success;
+                Mapping m;
+            };
+
+            /// \brief Returns a valid matching of \p h in \p g.
+            ///
+            /// Note that this is not necessairly an exact match (exact 
+            /// subgraph isomorphism). That is because this is a NP-Complete
+            /// problem.
+            virtual Result find(Graph::Ref g, Graph::Ref h) = 0;
+    };
+}
+
+#endif
---
+++ include/enfield/Transform/Allocators/WeightedSIMappingFinder.h
@@ -0,0 +1,32 @@
+#ifndef __EFD_WEIGHTED_SI_MAPPING_FINDER_H__
+#define __EFD_WEIGHTED_SI_MAPPING_FINDER_H__
+
+#include "enfield/Transform/Allocators/MappingFinder.h"
+#include "enfield/Support/SIFinder.h"
+#include "enfield/Support/WeightedGraph.h"
+
+namespace efd {
+    /// \brief Finds a mapping with a weighted partial matching algorithm (subgraph
+    /// isomorphism).
+    class WeightedSIMappingFinder : public MappingFinder {
+        public:
+            typedef uint32_t WeightTy;
+
+            typedef WeightedSIMappingFinder* Ref;
+            typedef std::unique_ptr<WeightedSIMappingFinder> uRef;
+
+        private:
+            SIFinder::sRef mSIFinder;
+
+        public:
+            Mapping find(ArchGraph::Ref g, DepsSet& deps) override;
+
+            /// \brief Sets the subgraph isomorphism finder to \p finder.
+            void setSIFinder(SIFinder::sRef finder);
+
+            /// \brief Creates an instance of this class.
+            static uRef Create();
+    };
+}
+
+#endif
