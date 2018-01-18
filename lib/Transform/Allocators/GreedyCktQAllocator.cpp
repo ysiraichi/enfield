283059f7a390174d921b059950a94e398b63054b
Yukio Siraichi <yukio.siraichi@gmail.com>
2018-01-18 17:20:29 +0100

    [GreedyCktQAllocator]: implemented frozen logic.
    As in the 'PathGuidedSolBuilder', we implemented the 'frozen' idea for
    this allocator. This idea comes from mapping dependencies directly if
    neither they nor the ones that they will move to have not been used yet.

--- lib/Transform/Allocators/GreedyCktQAllocator.cpp
+++ lib/Transform/Allocators/GreedyCktQAllocator.cpp
@@ -8,11 +8,24 @@
 
 using namespace efd;
 
+enum PropKind { K_SWP, K_FRZ } type;
+
 struct AllocProps {
     CircuitNode* cnode;
-    bool mvTgtSrc;
     uint32_t cost;
     std::vector<uint32_t> path;
+
+    PropKind type;
+
+    union {
+        struct {
+            bool mvTgtSrc;
+        } swp;
+
+        struct {
+            uint32_t from, to;
+        } frz;
+    } u;
 };
 
 GreedyCktQAllocator::GreedyCktQAllocator(ArchGraph::sRef ag) : QbitAllocator(ag) {}
@@ -41,7 +54,6 @@ Solution GreedyCktQAllocator::executeAllocation(QModule::Ref qmod) {
     std::vector<bool> frozen(mapping.size(), false);
 
     for (uint32_t t = 0; t < nofDepsSet; ++t) {
-
         bool changed;
         do {
             changed = false;
@@ -81,8 +93,35 @@ Solution GreedyCktQAllocator::executeAllocation(QModule::Ref qmod) {
             }
         }
 
+        // Removing instructions that don't use only one qubit, but do not have any dependencies
+        bool redo = false;
+        for (auto cnode : allocatable) {
+            auto node = cnode->node;
+            auto dep = depBuilder.getDeps(node);
+
+            if (dep.getSize() == 0) {
+                redo = true;
+                newNodes.push_back(node->clone());
+
+                for (uint32_t q : cnode->qargsid) {
+                    frozen[q] = true;
+                    marked[q] = false;
+                    cgraph[q] = cgraph[q]->child[q];
+                }
+
+                for (uint32_t c : cnode->cargsid) {
+                    marked[c] = false;
+                    cgraph[c] = cgraph[c]->child[c];
+                }
+            }
+        }
+
+        if (redo) { --t; continue; }
+
         assert(allocatable.size() >= 1 && "Every step has to be one allocatable node.");
-        AllocProps bestProps { nullptr, false, _undef, std::vector<uint32_t>() };
+        AllocProps best;
+        best.cnode = nullptr;
+        best.cost = _undef;
 
         for (auto cnode : allocatable) {
             // Calculate cost for allocating cnode->node;
@@ -91,72 +130,153 @@ Solution GreedyCktQAllocator::executeAllocation(QModule::Ref qmod) {
             auto dep = depBuilder.getDeps(node);
 
             assert(dep.getSize() <= 1 && "Can only allocate gates with at most one depenency.");
-            cnode->node->print(std::cout, true);
 
             uint32_t a = dep[0].mFrom, b = dep[0].mTo;
             uint32_t u = mapping[a], v = mapping[b];
 
-            auto bfspath = bfs.find(mArchGraph.get(), u, v);
-            uint32_t pathsize = bfspath.size();
-            AllocProps props { cnode, true, (pathsize - 2) * SwapCost.getVal(), bfspath };
+            AllocProps props;
+            props.cnode = cnode;
+            props.cost = 0;
+            props.path = {};
 
-            bool hasFrozenVertex = false;
-            for (uint32_t u : bfspath) {
-                if (frozen[u]) { hasFrozenVertex = true; break; }
+            // If either 'a' or 'b' is not frozen, we can search for a vertice nearby (each of them)
+            // in order not to o any swaps.
+            bool hasEdge = mArchGraph->hasEdge(u, v);
+            bool hasReverseEdge = mArchGraph->hasEdge(v, u);
+
+            if (hasEdge) {
+                props.type = K_SWP;
+            } else if (!hasEdge && hasReverseEdge) {
+                props.type = K_SWP;
+                props.cost = RevCost.getVal();
+            } else {
+                bool foundFrozen = false;
+
+                if (!frozen[a] || !frozen[b]) {
+                    uint32_t notFrozen, theOther;
+
+                    if (!frozen[b]) {
+                        notFrozen = b; theOther = a;
+                    } else {
+                        notFrozen = a; theOther = b;
+                    }
+
+                    // This will repeat two times (if both 'a' and 'b' are not frozen)
+                    while (!foundFrozen && !frozen[notFrozen]) {
+                        uint32_t u = mapping[theOther];
+
+                        for (uint32_t v : mArchGraph->adj(u)) {
+                            uint32_t otherNotFrozen = assign[v];
+
+                            if (!frozen[otherNotFrozen]) {
+                                props.type = K_FRZ;
+                                props.u.frz.from = notFrozen;
+                                props.u.frz.to = otherNotFrozen;
+
+                                if (!mArchGraph->hasEdge(u, mapping[otherNotFrozen]))
+                                    props.cost = RevCost.getVal();
+
+                                foundFrozen = true;
+                                break;
+                            }
+                        }
+
+                        std::swap(notFrozen, theOther);
+                    }
+                }
+
+                if (!foundFrozen) {
+                    props.type = K_SWP;
+
+                    auto bfspath = bfs.find(mArchGraph.get(), u, v);
+                    uint32_t pathsize = bfspath.size();
+
+                    props.path = bfspath;
+                    props.cost = (bfspath.size() - 2) * SwapCost.getVal();
+
+                    bool hasEdgeFromU = mArchGraph->hasEdge(bfspath[0], bfspath[1]);
+                    bool hasEdgeToV = mArchGraph->hasEdge(bfspath[pathsize - 2], bfspath[pathsize - 1]);
+                    if (hasEdgeFromU)
+                        props.u.swp.mvTgtSrc = true;
+                    else if (!hasEdgeFromU && hasEdgeToV)
+                        props.u.swp.mvTgtSrc = false;
+                    else if (!hasEdgeFromU && !hasEdgeToV)
+                        props.cost += RevCost.getVal();
+                }
             }
 
-            if (hasFrozenVertex) {
-                bool hasEdgeFromU = mArchGraph->hasEdge(bfspath[0], bfspath[1]);
-                bool hasEdgeToV = mArchGraph->hasEdge(bfspath[pathsize - 2], bfspath[pathsize - 1]);
-
-                if (!hasEdgeFromU && hasEdgeToV)
-                    props.mvTgtSrc = false;
-                else if (!hasEdgeFromU && !hasEdgeToV)
-                    props.cost += RevCost.getVal();
-
-                if (bestProps.cost > props.cost)
-                    bestProps = props;
-            }
+            if (best.cost > props.cost)
+                best = props;
         }
 
-        assert(bestProps.cnode != nullptr && "There must be a 'best' node.");
+        assert(best.cnode != nullptr && "There must be a 'best' node.");
 
         // Allocate best node;
         // Setting the 'stop' flag;
         auto& ops = sol.mOpSeqs[t];
-        auto node = bestProps.cnode->node;
+        auto node = best.cnode->node;
         auto newNode = node->clone();
 
         ops.first = newNode.get();
         newNodes.push_back(std::move(newNode));
 
-        if (bestProps.mvTgtSrc)
-            std::reverse(bestProps.path.begin(), bestProps.path.end());
+        if (best.type == K_SWP) {
+            if (best.u.swp.mvTgtSrc)
+                std::reverse(best.path.begin(), best.path.end());
 
-        for (auto it = bestProps.path.begin() + 1, end = bestProps.path.end(); it != end; ++it) {
-            uint32_t u = *(it - 1), v = *it;
-            uint32_t a = assign[u], b = assign[v];
+            if (best.path.size() > 2) {
+                for (auto it = best.path.begin() + 2, end = best.path.end();
+                        it != end; ++it) {
+                    uint32_t u = *(it - 2), v = *(it - 1);
+                    uint32_t a = assign[u], b = assign[v];
 
-            ops.second.push_back({ Operation::K_OP_SWAP, a, b });
+                    frozen[a] = true;
+                    frozen[b] = true;
 
-            std::swap(mapping[a], mapping[b]);
+                    ops.second.push_back({ Operation::K_OP_SWAP, a, b });
+                    std::cout << "SWAP(" << a << " => " << u << "; " << b << " => " << b << ")" << std::endl;
+
+                    std::swap(mapping[a], mapping[b]);
+                    std::swap(assign[u], assign[v]);
+                }
+            }
+        } else {
+            uint32_t a = best.u.frz.from, newA = best.u.frz.to;
+            uint32_t u = mapping[a], v = mapping[newA];
+            std::swap(sol.mInitial[a], sol.mInitial[newA]);
+            std::swap(mapping[a], mapping[newA]);
             std::swap(assign[u], assign[v]);
+
+            std::cout << "FRZ(" << a << " => " << mapping[a] << "; " << newA << " => " << mapping[newA] << ")" << std::endl;
         }
 
-        auto dep = depBuilder.getDeps(bestProps.cnode->node)[0];
+        // std::cout << "Cost: " << best.cost << std::endl;
+
+        auto dep = depBuilder.getDeps(best.cnode->node)[0];
         uint32_t a = dep.mFrom, b = dep.mTo;
 
-        if (mArchGraph->hasEdge(mapping[a], mapping[b]))
+        frozen[a] = true;
+        frozen[b] = true;
+
+        if (mArchGraph->hasEdge(mapping[a], mapping[b])) {
             ops.second.push_back({ Operation::K_OP_CNOT, a, b });
-        else
+            std::cout << "CNOT(" << a << " => " << mapping[a] << "; " << b << " => " << mapping[b] << ")" << std::endl;
+        } else {
             ops.second.push_back({ Operation::K_OP_REV, a, b });
+            std::cout << "REV(" << a << " => " << mapping[a] << "; " << b << " => " << mapping[b] << ")" << std::endl;
+        }
 
-        for (uint32_t q : bestProps.cnode->qargsid)
+        for (uint32_t q : best.cnode->qargsid) {
+            marked[q] = false;
             cgraph[q] = cgraph[q]->child[q];
-        for (uint32_t c : bestProps.cnode->cargsid)
-            cgraph[c] = cgraph[c]->child[c];
+        }
 
-        sol.mCost += bestProps.cost;
+        for (uint32_t c : best.cnode->cargsid) {
+            marked[c] = true;
+            cgraph[c] = cgraph[c]->child[c];
+        }
+
+        sol.mCost += best.cost;
     }
 
     qmod->clearStatements();
