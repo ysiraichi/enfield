a426fd0d39e19dd97e5447eed817b7c27f42a711
Yukio Siraichi <yukio.siraichi@gmail.com>
2018-01-23 18:03:44 +0100

    [Driver]: implemented.
    'Driver' functions implemented. Exposing methods for compiling (all the
    stuff necessary) and for reading a program from a file (logic for spliting
    the filepath into filename and path).
    'Enfield' using 'Driver'.

---
+++ lib/Transform/Driver.cpp
@@ -0,0 +1,91 @@
+#include "enfield/Transform/Driver.h"
+#include "enfield/Transform/FlattenPass.h"
+#include "enfield/Transform/XbitToNumberPass.h"
+#include "enfield/Transform/SemanticVerifierPass.h"
+#include "enfield/Transform/ArchVerifierPass.h"
+#include "enfield/Transform/CNOTLBOWrapperPass.h"
+#include "enfield/Transform/ReverseEdgesPass.h"
+#include "enfield/Transform/PassCache.h"
+#include "enfield/Transform/Allocators/Allocators.h"
+#include "enfield/Arch/Architectures.h"
+
+using namespace efd;
+
+QModule::uRef efd::Compile(QModule::uRef qmod, CompilationSettings settings) {
+    QModule::uRef qmodCopy;
+
+    if (settings.verify) {
+        qmodCopy = qmod->clone();
+    }
+
+    PassCache::Run<FlattenPass>(qmod.get());
+
+    if (settings.reorder) {
+        PassCache::Run<CNOTLBOWrapperPass>(qmod.get());
+    }
+
+    auto xbitPass = PassCache::Get<XbitToNumberWrapperPass>(qmod.get());
+    auto xbitToNumber = xbitPass->getData(); 
+
+    std::shared_ptr<ArchGraph> archGraph;
+    if (HasArchitecture(settings.architecture)) {
+        archGraph = CreateArchitecture(settings.architecture);
+    } else {
+        archGraph = ArchGraph::Read(settings.architecture);
+    }
+
+    assert(xbitToNumber.getQSize() <= archGraph->size() &&
+            "Using more qbits than the maximum permitted by the architecture.");
+
+    auto allocPass = CreateQbitAllocator(settings.allocator, archGraph);
+    allocPass->setInlineAll(settings.basis);
+    PassCache::Run(qmod.get(), allocPass.get());
+
+    auto revPass = ReverseEdgesPass::Create(archGraph);
+    PassCache::Run(qmod.get(), revPass.get());
+
+    if (settings.verify) {
+        auto mapping = allocPass->getData().mInitial;
+
+        auto aVerifierPass = ArchVerifierPass::Create(archGraph);
+        auto sVerifierPass = SemanticVerifierPass::Create(std::move(qmodCopy), mapping);
+        sVerifierPass->setInlineAll(settings.basis);
+
+        PassCache::Run(qmod.get(), aVerifierPass.get());
+        assert(aVerifierPass->getData() &&
+                "Architecture restrictions violated in compiled code.");
+
+        PassCache::Run(qmod.get(), sVerifierPass.get());
+        assert(sVerifierPass->getData() &&
+                "Compiled code is semantically different from source code.");
+    }
+
+    return qmod;
+}
+
+QModule::uRef efd::ParseFile(std::string filepath) {
+    QModule::uRef qmod(nullptr);
+    std::string path;
+    std::string filename;
+
+    if (filepath != "") {
+        // Spliting filepath into 'filename' and 'path'.
+        auto lastslash = filepath.find_last_of('/');
+
+        if (lastslash != std::string::npos) {
+            path = filepath.substr(0, lastslash + 1);
+            filename = filepath.substr(lastslash + 1, std::string::npos);
+        } else {
+            path = "./";
+            filename = filepath;
+        }
+
+        qmod.reset(QModule::Parse(filename, path).release());
+    }
+
+    return qmod;
+}
+
+void efd::PrintToStream(QModule::Ref qmod, std::ostream& o) {
+    qmod->print(o, true);
+}
