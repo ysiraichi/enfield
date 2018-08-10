10cc274f90fed2089bca30a8e3f96e688485c791
Yukio Siraichi <yukio.siraichi@gmail.com>
2018-08-10 22:29:11 +0200

    [JsonParser]: refactored.
    The idea is to centralize the creation of objects that use Json. Instead
    of calling `T::ReadString` for some class `T`, we would do it in a more
    transparent way: `JsonParse<T>::ParseString`.

--- include/enfield/Support/JsonParser.h
+++ include/enfield/Support/JsonParser.h
@@ -10,24 +10,64 @@ namespace efd {
     /// \brief Centralizes the field names for each type.
     template <class T> struct JsonFields {};
 
-    /// \brief Generates a \em Json::Value object from an input stream for
-    /// an object \em T.
-    ///
-    /// It allows some base checks too.
-    template <class T> struct JsonInputParser {
-        static Json::Value Parse(std::istream& in) {
-            Json::Value root;
-            in >> root;
-            return root;
+    /// \brief Returns a \em std::string corresponding to \p ty.
+    std::string JsonTypeString(const Json::ValueType& ty);
+
+    /// \brief Returns the \em std::string version of all \p tys for error messages.
+    std::string JsonTypeVectorString(const std::vector<Json::ValueType>& tys);
+
+    /// \brief Checks for a type error inside some key of the \em Json::Value.
+    template <class T>
+        void JsonCheckTypeError(const std::string prefix,
+                                const Json::Value v,
+                                const T key,
+                                const std::vector<Json::ValueType> tys) {
+            bool check = false;
+            auto keyTy = v[key].type();
+
+            for (auto ty : tys) {
+                if (keyTy == ty) {
+                    check = true;
+                    break;
+                }
+            }
+
+            if (!check) {
+                ERR << prefix << ": incorrect type for " << JsonTypeVectorString(tys) << ". "
+                    << "Actual: `" << JsonTypeString(keyTy) << "`." << std::endl;
+                ExitWith(ExitCode::EXIT_json_parsing_error);
+            }
+        }
+
+    /// \brief Gets a \em T instance (wrapped in a \em std::unique_ptr) from the \em Json::Value.
+    template <class T> struct JsonBackendParser {
+        static std::unique_ptr<T> Parse(const Json::Value& root) {
+            ERR << "Parse method not implemented for '" << typeid(T).name() << "'." << std::endl;
+            ExitWith(ExitCode::EXIT_unreachable);
         }
     };
 
-    /// \brief Gets a \em T instance (wrapped in a \em std::unique_ptr) from the \em Json::Value.
-    template <class T> struct FromJsonGetter {
-        static std::unique_ptr<T> Get(const Json::Value& root) {
-            ERR << "Get method not implemented for '" << typeid(T).name() << "'." << std::endl;
-            ExitWith(ExitCode::EXIT_unreachable);
-        }
+    /// \brief Frontend to the json parser.
+    template <class T> class JsonParser {
+        private:
+            static std::unique_ptr<T> ParseInputStream(std::istream& in) {
+                Json::Value root;
+                in >> root;
+                return JsonBackendParser<T>::Parse(root);
+            }
+
+        public:
+            /// \brief Parses a json \em std::string.
+            static std::unique_ptr<T> ParseString(std::string str) {
+                std::istringstream iss(str);
+                return ParseInputStream(iss);
+            }
+
+            /// \brief Parses a json file.
+            static std::unique_ptr<T> ParseFile(std::string filename) {
+                std::ifstream ifs(filename.c_str());
+                return ParseInputStream(ifs);
+            }
     };
 }
 
---
+++ lib/Support/JsonParser.cpp
@@ -0,0 +1,26 @@
+#include "enfield/Support/JsonParser.h"
+
+using namespace efd;
+
+std::string efd::JsonTypeString(const Json::ValueType& ty) {
+    switch (ty) {
+        case Json::ValueType::nullValue:    return "null";
+        case Json::ValueType::intValue:     return "int";
+        case Json::ValueType::uintValue:    return "uint";
+        case Json::ValueType::realValue:    return "real";
+        case Json::ValueType::stringValue:  return "string";
+        case Json::ValueType::booleanValue: return "boolean";
+        case Json::ValueType::arrayValue:   return "array";
+        case Json::ValueType::objectValue:  return "object";
+    }
+}
+
+std::string efd::JsonTypeVectorString(const std::vector<Json::ValueType>& tys) {
+    std::string str = "`" + JsonTypeString(tys[0]) + "`";
+
+    for (uint32_t i = 1, e = tys.size(); i < e; ++i) {
+        str += " or `" + JsonTypeString(tys[i]) + "`";
+    }
+
+    return str;
+}
