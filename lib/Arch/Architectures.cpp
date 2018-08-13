6981a456fd8fadbb01704aee88696b631fc4265f
Yukio Siraichi <yukio.siraichi@gmail.com>
2018-08-13 21:57:43 +0200

    [Architectures]: refactored.
    Using `JsonParser` for parsing the default architectures.

--- lib/Arch/Architectures.cpp
+++ lib/Arch/Architectures.cpp
@@ -1,68 +1,29 @@
 #include "enfield/Arch/Architectures.h"
 #include "enfield/Analysis/Nodes.h"
+#include "enfield/Support/JsonParser.h"
 #include "enfield/Support/RTTI.h"
 
 namespace efd {
     typedef std::shared_ptr<ArchRegistry> ArchRegistryPtr;
 
-#define EFD_ARCHITECTURE(_Name_, _QbitNum_) \
-    class Arch##_Name_ : public ArchGraph {\
-        public:\
-            static std::unique_ptr<Arch##_Name_> Create() {\
-                return std::unique_ptr<Arch##_Name_>(new Arch##_Name_());\
-            }\
-        private:\
-            Arch##_Name_() : ArchGraph(_QbitNum_, false) {\
-                uint32_t u, v;
-
-#define EFD_REG(_QReg_, _Size_) \
-                this->putReg(#_QReg_, #_Size_);\
-                auto ndID = NDId::Create(#_QReg_);\
-                for (uint32_t i = 0; i < _Size_; ++i) {\
-                    auto ndN = NDInt::Create(std::to_string(i));\
-                    auto ndIDCpy = dynCast<NDId>(ndID->clone().release());\
-                    this->putVertex(NDIdRef::Create\
-                            (NDId::uRef(ndIDCpy), std::move(ndN)));\
-                }
-
-#define EFD_COUPLING(_QReg_, _U_, _V_) \
-                u = this->getUId(#_QReg_"["#_U_"]");\
-                v = this->getUId(#_QReg_"["#_V_"]");\
-                this->putEdge(u, v, 1);
-
-#define EFD_ARCHITECTURE_END \
-            }\
-    };
-
-// From a previous configuration file '.def', by including it, it'll generate
-// the declaration of the architectures.
-#include "enfield/Arch/IBMQX2.def"
-#include "enfield/Arch/IBMQX3.def"
-
-#undef EFD_ARCHITECTURE
-#undef EFD_REG
-#undef EFD_COUPLING
-#undef EFD_ARCHITECTURE_END
-
-template <> std::vector<std::string> efd::EnumArchitecture::mStrVal {
-#define EFD_FIRSTLAST(_First_, _Last_)
-#define EFD_ARCH(_Arch_, _Name_) \
-    "A_"#_Name_,
+    template <> std::vector<std::string> efd::EnumArchitecture::mStrVal {
+        "None",
+#define EFD_ARCH(_Name_, _Json_) \
+        "A_"#_Name_,
 #include "enfield/Arch/Architectures.def"
-#undef EFD_FIRSTLAST
 #undef EFD_ARCH
-};
+        "None"
+    };
 
 // Creating functions for each architecture, so that it
 // will get the following signature: (int) -> ArchGraph*
-#define EFD_FIRSTLAST(_First_, _Last_)
-#define EFD_ARCH(_Arch_, _Name_) \
-    ArchRegistry::RetTy CreateArch##_Arch_(int pad) {\
-        auto arch = Arch##_Arch_::Create();\
+#define EFD_ARCH(_Name_, _Json_) \
+    ArchRegistry::RetTy CreateArch_##_Name_(int pad) {\
+        auto json = std::string(#_Json_);\
+        auto arch = JsonParser<ArchGraph>::ParseString(json.substr(1, json.length() - 2));\
         return ArchRegistry::RetTy(arch.release());\
     }
 #include "enfield/Arch/Architectures.def"
-#undef EFD_FIRSTLAST
 #undef EFD_ARCH
 }
 
@@ -72,11 +33,9 @@ static efd::ArchRegistryPtr GetRegistry() {
 }
 
 void efd::InitializeAllArchitectures() {
-#define EFD_FIRSTLAST(_First_, _Last_)
-#define EFD_ARCH(_Arch_, _Name_) \
-    RegisterArchitecture(Architecture::A_##_Name_, CreateArch##_Arch_);
+#define EFD_ARCH(_Name_, _Json_) \
+    RegisterArchitecture(Architecture::A_##_Name_, CreateArch_##_Name_);
 #include "enfield/Arch/Architectures.def"
-#undef EFD_FIRSTLAST
 #undef EFD_ARCH
 }
 
